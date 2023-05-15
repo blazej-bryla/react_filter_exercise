a3fa56ca71699212f55ccaf8ac490f131942b999
blazej-bryla <brykuski@gmail.com>
2023-05-15 14:56:48 +0200

    create filter funciton and filter input

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { updateQuery } from '../redux/people/actions';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { updateQuery } from "../redux/people/actions";
 function Filter() {
+  const dispatch = useDispatch();
 
-  return (
-    <div className='App-box'>
-    </div>
-  );
+  const handleChanges = (event) => {
+    dispatch(updateQuery(event.target.value))
+  }
+  return <div className="App-box">
+    <input type="text" placeholder="Search" onChange={(e) => handleChanges(e)} />
+  </div>;
 }
 
-Filter.propTypes = {
-};
+Filter.propTypes = {};
 
 export default Filter;
